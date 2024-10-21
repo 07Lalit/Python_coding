bc6925d4ed806b0dd4d94f5963a8957c442810bc
Lalit <90924706+07Lalit@users.noreply.github.com>
2024-10-21 21:51:09 +0530

    Add files via upload

---
+++ functions_in_python_day13.py
@@ -0,0 +1,166 @@
+"""
+Q1. What is Functions in python ?
+-> It is a named Piece of code which can be used as
+   many time as we want to perform a specific task.
+
+   In python we used a keyword called "def" to make
+   function .
+
+    If we want to use than we have to call name
+    of the function
+
+    def add(a,b):
+        return a+b
+
+    print(add(10,20))  # 30
+
+--------------------------------------------------------
+
+                TYPES OF FUNCTION
+                ----------------
+
+-> It is of 3 types-:
+    1) Built-in function .
+    2) module level function .
+    3) user - defined fucntion.
+
+ 1) Built-in function -: Create by programming language
+         for general use .
+    -> We don't need to create we just have to use it.
+    -> They are also known as global function
+    -> 69+ (built in function in python).
+
+    Built-in Functions
+
+    A
+    abs()
+    aiter()
+    all()
+    anext()
+    any()
+    ascii()
+
+    B
+    bin()
+    bool()
+    breakpoint()
+    bytearray()
+    bytes()
+
+    C
+    callable()
+    chr()
+    classmethod()
+    compile()
+    complex()
+
+    D
+    delattr()
+    dict()
+    dir()
+    divmod()
+
+    E
+    enumerate()
+    eval()
+    exec()
+
+    F
+    filter()
+    float()
+    format()
+    frozenset()
+
+    G
+    getattr()
+    globals()
+
+    H
+    hasattr()
+    hash()
+    help()
+    hex()
+
+    I
+    id()
+    input()
+    int()
+    isinstance()
+    issubclass()
+    iter()
+    L
+    len()
+    list()
+    locals()
+
+    M
+    map()
+    max()
+    memoryview()
+    min()
+
+    N
+    next()
+
+    O
+    object()
+    oct()
+    open()
+    ord()
+
+    P
+    pow()
+    print()
+    property()
+
+
+
+
+    R
+    range()
+    repr()
+    reversed()
+    round()
+
+    S
+    set()
+    setattr()
+    slice()
+    sorted()
+    staticmethod()
+    str()
+    sum()
+    super()
+
+    T
+    tuple()
+    type()
+
+    V
+    vars()
+
+    Z
+    zip()
+
+    _
+    __import__()
+
+    https://docs.python.org/3/library/functions.html
+
+
+    kisi bhi name ke aage agar paranthesis laga ho to
+    ushe function bolte hai.
+
+    ------------------------------------------------------------------------------
+
+                              2) Module -level function
+                              -------------------------
+
+  ->  The fucntion cannot be us used directly
+
+
+"""
+# def add(a, b):
+#     return a + b
+#
+# print(add(10, 20))
