c5529f86fe71cf26332b7bf884366f37df57ff3e
Lalit <90924706+07Lalit@users.noreply.github.com>
2024-11-19 21:36:32 +0530

    Add files via upload

---
+++ python_fundamental_flow_control.py
@@ -0,0 +1,104 @@
+"""
+               Flow control
+               ------------
+
+ ->    Flow control describes that order in which statement excutes at runtime.
+
+  1) Conditional statement :
+   if , elif , else .
+
+
+  2) Transfer statement-:
+  break , continue ,pass
+
+
+  3) Iterative statements
+  while , for loop .
+
+  ------------------------------------------------------------------------
+
+                     A) Conditional statement
+                     ---------------------------
+    if -> agar
+    else -> yaa to / nahi to
+
+    use -: used in decision making .
+
+    1)  if is a keyword in python  :
+      syntax-: if (condition):
+                   prin
+                   fijsgj
+                   fkjg
+               else:
+                   rpitn;k
+
+    -> parenthesis is optional in condtion .
+      we must give 4 space identation to make a block of code .
+
+
+
+
+"""
+# a =  int(input(" Enter a number: "))
+# if (a%2==0):
+#     print("even")
+# else:
+#     print("odd")
+
+# num = int(input("Enter a number btw 1-7 :  "))
+# if (num==1):
+#     print("Monday")
+# elif (num==2):
+#     print("Tuesday")
+# elif (num==3):
+#     print("wednesday")
+# elif (num==4):
+#     print("Thursday")
+# elif (num==5):
+#     print("Friday")
+# elif (num==6):
+#     print("saturday")
+# elif (num==7):
+#     print("Sunday")
+# else:
+#     print("You entered a wrong input..!!")
+
+
+
+# num = int(input("enter a number  "))
+# print("squar of this number: ", num*num)
+# print(f"cube of this number: {num*num*num}")
+
+
+# age = int(input("Enter a number  "))
+# if (age>=18):
+#     print("he is eligible for vote ")
+# else:
+#     print("he is not eligible for vote ")
+
+
+# age = int(input("Enter a number "))
+# if(age<0):
+#     print("incorrect age",chr(128514))
+# elif(age<18):
+#     print("minor",chr(128514))
+# elif(age>18):
+#     print("adult",chr(128515))
+# else:
+#     print("you are old")
+
+
+# for i in range(1,11):
+#     print(f" 2*{i} = {2 * i}")
+
+
+# a = int(input("enter a number one:  " ))
+# b = int(input("enter a number two:  " ))
+# c = int(input("enter a number three:  " ))
+
+# if(a>=b and a>=c):
+#     print("largest a")
+# elif(b>=a and b>=c):
+#     print("largest b")
+# else:
+#     print("largest c")
