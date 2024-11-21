8d6fb259ab49515c294d9d86cbbffa1bde924178
Lalit <90924706+07Lalit@users.noreply.github.com>
2024-11-21 21:46:49 +0530

    Add files via upload

---
+++ python_fundamentals_loop_class1.py
@@ -0,0 +1,35 @@
+"""
+flow control-:
+                           2) Iterative statement
+                            --------------------
+
+-> Q Write a program print your name 10 times on console scree ?
+-> print("Lalit")
+
+-> This statement are used to execute a piece of code multiple times in our
+   program .
+
+   1) using while loop ->
+
+   2) using for loop ->
+
+    loops are those iterative statement which executes a code multiple time .
+
+Types of loop
+-------------
+1) Entry  control loop -:  for loop , while loop
+2) exit control loop -: do while loop (c, c++ ,java)
+
+
+
+
+
+"""
+i=1
+while(i<10):
+    print("hello world")
+    i=i+1;
+    
+
+
+
