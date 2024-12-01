df93b8f3f6835d59028a1d80c300143bd68864e2
Lalit <90924706+07Lalit@users.noreply.github.com>
2024-12-01 22:02:48 +0530

    Add files via upload

---
+++ python_flow_control_for_loop.py
@@ -0,0 +1,56 @@
+"""
+                     For loop
+                     ---------
+
+-> In c,c++,java :
+    for(i=0;i<=10;i++){
+        printf("hello world");
+    }
+
+-> The for loop in python is quite different as compare
+   to other programming languages like c,c++,java etc.
+-> The type of for loop works / iterate over a collection
+   of item (sequence)
+   -> String
+   -> list
+   -> tuple
+   -> range
+   -> byte
+   -> byte array
+
+
+-> collection of items means any iterable/sequence/collection
+
+  we can use range datatype in python if we know that how many
+  times we have to iterate upon a block of code.
+
+  range(start ,ending value,step)
+
+  -> It is a sequece datatype
+  -> range() is also a function which is defined inside a
+     range class .
+
+ -> range() function generate and return an collection of item .
+
+ -> that's why it is used in for loop .
+
+ range(n)  -> zero to n-1
+ range(s,e) -> starting to ending -1
+ range(starting ,ending,step)
+
+
+ Important points-:
+
+ 1) Range fucntion always take integer in parmeter.
+ 2) range(6) -> 0,1,2,3,4,5
+ 3) range(1,10) -> 1,2,3,4,5,6,7,8,9
+ 4) range(1,10,2) -> 1,3,5,7,9
+ 5) range(10,1,-1) -> 10,9,8,7,6,5,4,3,2,1
+
+3) It should always be positive when there is one value in range in function .
+
+
+
+
+
+"""
