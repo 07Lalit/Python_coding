e8abca66f0ab8eeeb2a09d837840e4124b127be4
Lalit <90924706+07Lalit@users.noreply.github.com>
2024-12-08 14:22:39 +0530

    Add files via upload

--- python_pattern_printing_nested_loop.py
+++ python_pattern_printing_nested_loop.py
@@ -204,12 +204,166 @@ for i in range(1,n+1):
 
 
 """
+Q10 WAP to print pattern like this ?
+
+1 2 3 4 5
+1 2 3 4
+1 2 3 
+1 2 
+1
+
+for i in range(5,0,-1):
+    for j in range(i,0,-1):
+        print(j,end=" ")
+    print()
+
+"""
+
+"""
+Q11 WAP to print pattern like this ? 
+
+_ _ _ _ * 
+_ _ _ * * 
+_ _ * * * 
+_ * * * * 
+* * * * * 
+
+n=5
+for i in range(1,n+1):
+    for j in range(n-i):
+        print(" ",end=" ")
+    for k in range(i):
+        print("*",end=" ")
+    print()
 
 """
 
 
+"""
+
+Q12  WAP to print pattern like this ?
+
+ _ _ _ _ *
+ _ _ _ * * * 
+ _ _ * * * * * 
+ _ * * * * * * * 
+ * * * * * * * * * 
+n=5
+for i in range(0,n):
+    for j in range(0,n-i-1):
+        print(" ",end=" ")
+    for k in range(i+1):
+        print("*",end=" ")
+    for m in range(i):
+        print("*",end=" ")
+    print()
+
+"""
+
+"""
+
+Q13 WAP to print pattern like this ?
+
+* * * * * * * * *
+_ * * * * * * * 
+_ _ * * * * * 
+_ _ _ * * * 
+_ _ _ _ * 
 
 
+
+n = 5
+for i in range(n,0,-1):
+    for j in range(n-i):
+        print("_",end=" ")
+    for k in range(i):
+        print("*",end=" ")
+    for l in range(i-1):
+        print("*",end=" ")
+    print()
+
+"""
+
+"""
+Q14 WAP to print pattern like this ?
+
+                  * 
+                * * * 
+              * * * * * 
+            * * * * * * * 
+          * * * * * * * * * 
+        * * * * * * * * * * * 
+      * * * * * * * * * * * * * 
+    * * * * * * * * * * * * * * * 
+  * * * * * * * * * * * * * * * * * 
+* * * * * * * * * * * * * * * * * * * 
+* * * * * * * * * * * * * * * * * * * 
+  * * * * * * * * * * * * * * * * * 
+    * * * * * * * * * * * * * * * 
+      * * * * * * * * * * * * * 
+        * * * * * * * * * * * 
+          * * * * * * * * * 
+            * * * * * * * 
+              * * * * * 
+                * * * 
+                  * 
+
+a = int(input(" Enter a number : "))
+n = a//2
+for i in range(0,n):
+    for j in range(0,n-i-1):
+        print(" ",end=" ")
+    for k in range(i+1):
+        print("*",end=" ")
+    for m in range(i):
+        print("*",end=" ")
+    print()
+
+for i in range(n,0,-1):
+    for j in range(n-i):
+        print(" ",end=" ")
+    for k in range(i):
+        print("*",end=" ")
+    for l in range(i-1):
+        print("*",end=" ")
+    print()
+
+"""
+
+"""
+Q15 WAP to print pattern like this ?
+
+* 
+* * 
+* * * 
+* * * * 
+* * * * * 
+* * * * * 
+* * * * 
+* * * 
+* * 
+* 
+
+
+n=10
+for i in range(1,(n//2) +1):
+    for j in range(i):
+        print("*",end=" ")
+    print()
+for i in range(n//2,0,-1):
+    for j in range(i):
+        print("*",end=" ")
+    print()
+    
+"""
+
+for i in range(0,5):
+    for j in range(0,i+1):
+        if((i==j) or  ((i+j)%2 == 0) ):
+            print("1",end=" ")
+        else:
+            print("0",end=" ")
+    print()
 
 
 
