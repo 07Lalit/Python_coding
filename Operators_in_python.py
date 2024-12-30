19b425d9df509183b517d1b2e57b9bc61010d0ce
Lalit <90924706+07Lalit@users.noreply.github.com>
2024-12-30 21:44:01 +0530

    Add files via upload

--- Operators_in_python.py
+++ Operators_in_python.py
@@ -98,8 +98,82 @@ ________________________________________________________________________________
 -> PEP( python Enhancement proposal)
   # It is a type of rule book of python (psf or develper of python)
   # It is a python style guide .
-  # Website -: developed by psf which tell's which things are recommendation by tpython .
+  # Website -: developed by psf which tell's which things are recommendation by python.
 
+-> For us it is multiline comment but internally for python it is an unrefrenced multiline
+   string literal .(such string object which do not have any reference ). It get loaded in heap
+   area as a string class object but it's reference count is zero that's why it is immediately
+   clean by garbage collector . But actually these lines are not ignored by compiler and
+   Interpreter . It just increase the overhead ( extra workload)  of python that's why thes comments
+   are not recommended by PEP (python enhancement proposal) .
+
+
+___________________________________________________________________________________________________________________
+
+                                 2) Relational / comparison operator
+                                 -------------------------------------
+
+->  The operators under relational are -:
+1) Greater than  -> ">"
+2) Less than     -> "<"
+3) Greater than equal to -> ">="
+4) Less than equal to     -> "<="
+5) Equals too -> "=="
+6) Not Equals too -> "!="
+
+-> They always return answers in boolean form that's True or False .
+-> They are used to compare between two values . (values must of same type for comparison)
+-> This operator can be also used for string .
+-> We cannot use relational operator to compare complex values .
+
+  Comaparison operator ->  "<" , ">" , ">=" , "<=" .
+  Equaltity operator -> "==" , "!="  .
+
+-> str to int comparison is not possible .
+   "kanchan" == 4959    # so This is not possible we will get error.
+
+-> Equality operator can work for different datattype also but if
+   we want to compare them it is not possible.
+
+_______________________________________________________________________________
+
+                    String comparison
+                    -----------------
+
+-> Comparison of string is not based on its length
+   "abc"
+   "abcdef"
+
+-> Each and every letter of both the string are compared by its
+   unicode value or Ascii .
+
+-> This character by character comparison by using unicode is
+   known as LEXOGRAPHICAL Comparison .
+
+some common unicode values you need to remember they are -:
+1) " " -> 32
+2) 0-9 -> 48 to 57
+3) A-Z -> 65 to 90
+4) a-z -> 97 to 122
+
+
+examples
+print(10>20>5)
+print(1>3>9>11<0)
+print("A"=="A" != "a" > "123")
+
+-> In arithematic operator chainning if any of the comparison result is False
+   then whole result is False .
+
+-> When all comparison is True then result is True.
+
+
+__________________________________________________________________________________________________________________________________
+
+                                   3) Logical operator
+                                   ----------------------
+
+-> So under logical operator there are -
 
 
 
@@ -108,23 +182,41 @@ ________________________________________________________________________________
 """
 #print(2.0//2)
 #print(22/7)
-a= 121
-b= 200
-c = "11"
-d= "22"
-e = "la"
-f ="lit"
-print(a+b ,type(a), type(b))
-print(c+d , type(c) ,type(d))
-print(e+f , type(e) , type(f))
+#a= 121
+#b= 200
+#c = "11"
+#d= "22"
+#e = "la"
+#f ="lit"
+# g="krish "
+# print(a+b ,type(a), type(b))
+# print(c+d , type(c) ,type(d))
+# print(e+f , type(e) , type(f))
+# print(g*3)
+# a ="*"
+# for i in range(1,6):
+#     print(i*a)
+#
+#
+# n = int(input("Enter a number till you want sum of n nutural number : "))
+# print((n*(n+1))//2)
 
-a ="*"
-for i in range(1,6):
-    print(i*a)
+a= "lalit"
+b= "himanshu"
+c = "krish"
+e = "kanchan"
+f = "Anushka"
+print(a!=f)
+print(a>b>c>e>f)
 
+print(10>20>5)  #
+print(1>3>9>11<0)
+print("A"=="A" != "a" > "123")
 
-n = int(input("Enter a number till you want sum of n nutural number : "))
-print((n*(n+1))//2)
+print(100 == "yash")
+
+print("101" <= 101)
+
 
 
 
