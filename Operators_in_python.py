f8b8450e2fe81fa9adb3b86a38df5d8781186fbb
Lalit <90924706+07Lalit@users.noreply.github.com>
2025-01-04 21:48:18 +0530

    Add files via upload

--- Operators_in_python.py
+++ Operators_in_python.py
@@ -52,172 +52,223 @@ vii)  '**'  -> power Operator .
 -> python always convert from Integer to float but vice versa is not possible .
    because of data lose until programmer force to convert it .
 
--> If one value is float all calculation is in float .
-   ex-:  10.2/2 => 5.0
-   ex-:  10.2//2 => 5.0
-
--> '/' -> return float till 16 digit precision .
-
--> Arithemetic opearator can be used for all Number type (int,float , complex) .
-
--> + & * these two operators can also used on strings .
+-> If one val
 
 
 
-ex-: a ="101"
-     b = "La"
-     c = "303"
-     d = "lit"
-
-     print(a+c)
-     print(b+d)
-
--> Arithametic operator can be used for all Numbertype (int,float , complex)
--> "+" & "*" these two operators can also used on strings.
--> if we add two string using + operator than in programming we call it we have done
-   concationation of two strings .
-
--> "*" -> string multiplication operator or normal multiplication operator
--> '+" -> string concatination operator .
-
-
-___________________________________________________________________________________________
-
-                         comments in python
-                         ------------------
-
--> In python there are two types of comments
-  1) Single line comments .
-  2) Multi line comments .
-
-1) Single line comments -> Single line comment is the official way of comment (rocommendation)
-                          by python .
-
-2) Multiline comment is unofficial way of comment (not recommended)
-
--> PEP( python Enhancement proposal)
-  # It is a type of rule book of python (psf or develper of python)
-  # It is a python style guide .
-  # Website -: developed by psf which tell's which things are recommendation by python.
-
--> For us it is multiline comment but internally for python it is an unrefrenced multiline
-   string literal .(such string object which do not have any reference ). It get loaded in heap
-   area as a string class object but it's reference count is zero that's why it is immediately
-   clean by garbage collector . But actually these lines are not ignored by compiler and
-   Interpreter . It just increase the overhead ( extra workload)  of python that's why thes comments
-   are not recommended by PEP (python enhancement proposal) .
-
-
-___________________________________________________________________________________________________________________
-
-                                 2) Relational / comparison operator
-                                 -------------------------------------
-
-->  The operators under relational are -:
-1) Greater than  -> ">"
-2) Less than     -> "<"
-3) Greater than equal to -> ">="
-4) Less than equal to     -> "<="
-5) Equals too -> "=="
-6) Not Equals too -> "!="
-
--> They always return answers in boolean form that's True or False .
--> They are used to compare between two values . (values must of same type for comparison)
--> This operator can be also used for string .
--> We cannot use relational operator to compare complex values .
-
-  Comaparison operator ->  "<" , ">" , ">=" , "<=" .
-  Equaltity operator -> "==" , "!="  .
-
--> str to int comparison is not possible .
-   "kanchan" == 4959    # so This is not possible we will get error.
-
--> Equality operator can work for different datattype also but if
-   we want to compare them it is not possible.
-
-_______________________________________________________________________________
-
-                    String comparison
-                    -----------------
-
--> Comparison of string is not based on its length
-   "abc"
-   "abcdef"
-
--> Each and every letter of both the string are compared by its
-   unicode value or Ascii .
-
--> This character by character comparison by using unicode is
-   known as LEXOGRAPHICAL Comparison .
-
-some common unicode values you need to remember they are -:
-1) " " -> 32
-2) 0-9 -> 48 to 57
-3) A-Z -> 65 to 90
-4) a-z -> 97 to 122
-
-
-examples
-print(10>20>5)
-print(1>3>9>11<0)
-print("A"=="A" != "a" > "123")
-
--> In arithematic operator chainning if any of the comparison result is False
-   then whole result is False .
-
--> When all comparison is True then result is True.
-
-
-__________________________________________________________________________________________________________________________________
-
-                                   3) Logical operator
-                                   ----------------------
-
--> So under logical operator there are -
-
-
-
+___________________________________________________________________________________________________
 
 
 """
-#print(2.0//2)
-#print(22/7)
-#a= 121
-#b= 200
-#c = "11"
-#d= "22"
-#e = "la"
-#f ="lit"
-# g="krish "
-# print(a+b ,type(a), type(b))
-# print(c+d , type(c) ,type(d))
-# print(e+f , type(e) , type(f))
-# print(g*3)
-# a ="*"
-# for i in range(1,6):
-#     print(i*a)
+                    # 3) Logical operator
+                    # -------------------------
+
+#  and -> If first condition is false than it will not check the second cond .
+#         ex-: 10>5 and 5<10
+#
+#  or -> If first condition is True than it will not check for second
+#       condition.
+#
+# not -> not operator works on only one operand .
+#
+#       a = 10>5
+#       print(not(a))
 #
 #
-# n = int(input("Enter a number till you want sum of n nutural number : "))
-# print((n*(n+1))//2)
+# _____________________________________________________________________________________________
+#
 
-a= "lalit"
-b= "himanshu"
-c = "krish"
-e = "kanchan"
-f = "Anushka"
-print(a!=f)
-print(a>b>c>e>f)
+"""
 
-print(10>20>5)  #
-print(1>3>9>11<0)
-print("A"=="A" != "a" > "123")
+"""
+"""
+                    3) Logical operator 
+                    -------------------------
 
-print(100 == "yash")
+ and -> If first condition is false than it will not check the second cond .
+        ex-: 10>5 and 5<10 
 
-print("101" <= 101)
+ or -> If first condition is True than it will not check for second 
+      condition. 
+
+not -> not operator works on only one operand .
+
+      a = 10>5 
+      print(not(a)) 
 
 
+_____________________________________________________________________________________________
+
+               4) Assignment operator "=" 
+               ------------------------------ 
+
+-> We use this operator put / assign value to a variable . 
+
+ex-:  
+
+a = 10 
+b = 20.4 
+c = True 
+d = 45.6+8j 
+e = [1,2,3,4] 
+f = "python" 
+g = {1:"anushka" , 2: "krish"} 
+
+s = (1,2,3,4)
+
+
+python does not have increment and decrement operator .
+
+
+              compound assigemnet operator / shorthand operator 
+              --------------------------------------------------
+
+-> a = a*10        ->   a*=10  
+-> b = b//10       ->   b//=10 
+-> c = c+10        ->   c+=10 
+-> d = d+1         ->   d+=1
+i++ j++ i-- j--  python doesn't support this operator 
+
+
+
+______________________________________________________________________________________________________
+
+
+
+                     5) Bitwise Operator 
+                     --------------------- 
+
+-> It works on bits .
+  { 8 bits = 1 byte 
+   1024 byte = 1kb 
+   1024 kb  =  1 MB  
+   1024 MB  =  1 GB 
+   1024 GB  =  1 TB 
+   1024 TB  =  1 PB 
+   1024 PB  =  1 EB 
+   1024 EB  =  1 ZB 
+   1024 ZB =   1 YB 
+   }
+
+ -> This is applicable only for int and boolean data type .
+
+ -> If other datatype -> typeError 
+
+ -> Types of Bitwise operator -: 
+   I)    &  -> Bitwise and 
+
+   II)   |  -> Bitwise or 
+
+   III)  ^  -> Bitwise xor 
+
+   IV)   ~  -> Bitwise not 
+
+   V)    >> -> Bitwise right shift 
+
+   VI)   << -> Bitwise left shift 
+
+
+-> Fast ( it is easy to handle because in bits 0,1 ) in execution time .
+
+
+-------------------------------------------------------------------------------------
+
+                         I) Bitwise and  "&" 
+                        ----------------------
+
+-> Convert number into binary . If bits are one than ans will be one otherwise zero .
+
+   a = 14 
+   b = 10 
+
+
+a=35 
+b= 40 
+print(a&b)
+
+a=139
+b=197 
+print(a&b)
+
+
+______________________________________________________________________________________________________________________
+
+
+                        II) Bitwise or  
+                        ---------------
+
+-> If Bitwise of a|b if one of the  bits are one then answer will be one .
+
+  1 or 0  -> 1 
+  0 or 1  -> 1 
+  1 or 1  -> 1 
+  0 or 0  -> 0  
+
+
+
+
+
+a= 77 
+b = 99 
+print(a|b)
+
+c = 159 
+d = 229 
+print(bin(c),bin(d)
+
+
+___________________________________________________________________________________________________________________ 
+
+                        III) Bitwise xor operator 
+                        -------------------------- 
+
+-> xor operator -> a^b . If both bits is same than answer is zero (0) otherwise (1) .
+
+ Truth table of xor operator -> 
+
+ 1 xor 1  -> 0 
+ 0 xor 0  -> 0 
+ 1 xor 0  -> 1 
+ 0 xor 1  -> 1   
+
+
+"""
+
+a = 10
+b = 15
+print(a ^ b)
+
+print(21 ^ 18)
+
+# print(True or False)
+
+# print(True and True)
+
+# if 0.0 and "lalit":
+#     print("True")
+# else:
+#     print("False")
+
+# if "" or 0.1:
+#     print("True")
+# else:
+#     print("False")
+
+# if not "lalit":
+#     print("True")
+# else:
+#     print("False")
+
+# if( "himanshu" or "0") and (0.0 and 1.0) :
+#     print("True")
+# else:
+#     print("False")
+
+
+# if "0" and 1.0:
+#     print("True")
+# else:
+#     print("False")
 
 
 
